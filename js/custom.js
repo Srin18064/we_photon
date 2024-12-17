bde5b1d9fd65fc791e1a54e8d31f321807a3bc07
Srinivasan Mani <linuxosuser0@gmail.com>
2024-12-17 09:29:18 +0530

    final corrections1

--- js/custom.js
+++ js/custom.js
@@ -141,3 +141,19 @@ window.onclick = function(event) {
     imagePopup.style.display = "none"; // Hide the popup
   }
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
