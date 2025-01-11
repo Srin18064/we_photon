9a436aeee8159ef91a75f7c304a357b5a450fb07
Srinivasan Mani <linuxosuser0@gmail.com>
2025-01-11 19:25:12 +0530

    changing tel was mail red

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
-/* Author:WebThemez
- * Author URI:http://webthemez.com
+/* Author:Wephoton
+ * Author URI:http://wephoton.in
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  */
 
